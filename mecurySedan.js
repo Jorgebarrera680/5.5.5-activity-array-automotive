ee45d2ae2b3b84f25c0c007ece542ccaf5b41a63
Jorgebarrera680 <106217759+Jorgebarrera680@users.noreply.github.com>
2022-08-14 21:30:51 -0500

    Update mecurySedan.js

--- mecurySedan.js
+++ mecurySedan.js
@@ -4,3 +4,56 @@ const VehicleModule = require("./vehicleBaseClass")
 //this shows how to call from this module...
 let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
 console.log(v.make)
+
+
+class car extends VehicleModule{
+    constructor(Make,vehicleType, year, color, milage){
+        this.make = make;
+        this.vehicleType = vehicleType;
+        this.year = year;
+        this.color = color;
+        this.milage = milage;
+
+        this.numberOfPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+
+
+    }
+}
+
+    loadPassenger(num); {
+        if (this.numberOfPassengers < this.passenger) {
+            console.log("Room available");
+
+        
+        } else{
+            console.log(this.year + " "+ this.model + "room not available");
+        }
+
+    }
+
+
+    start();{
+    if (this.fuel === 0){
+        console.log("engine started");
+    }
+    else{
+        console.log("no fuel");
+    }
+    
+
+    }
+
+    maintnace();{
+        if(this.milage > 3000){
+            console.log("maintnace required");
+        } else{
+            console.log("not time for maintnace")
+        }
+    }
+
+    
